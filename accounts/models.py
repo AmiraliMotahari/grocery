5a42649525cc8c773c1746f9d7a7f0cbf6f99888
Amirali Motahari <Amiralimotahari.dev@outlook.com>
2023-06-29 16:19:37 +0330

    v0.0.10

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,19 @@
+from audioop import reverse
 from django.contrib.auth.models import AbstractUser 
 from django.db import models
 from shop.models import City
+
 
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    city = models.ForeignKey(City,default='', null=True, blank=False ,on_delete=models.CASCADE)
+    city = models.ForeignKey(City, default='', null=True, blank=False ,on_delete=models.CASCADE)
     
     def __str__(self): 
-        return self.city
+        return str(self.city)
+    
+    def get_absolute_url(self):
+        return reverse('home')
+    
     
